f8793895d40f2eff65a51f3faff0fc2922aa161f
Anirudh906 <40207836+Anirudh906@users.noreply.github.com>
2021-09-27 10:20:20 +0530

    first release

---
+++ cli.js
@@ -0,0 +1,8 @@
+#! /usr/bin/env node
+
+var quoter = require("./index");
+
+var arg = process.argv[2];
+
+if (arg == "--offline") quoter(false);
+else quoter(true);
--- index.js
+++ index.js
@@ -1,24 +1,114 @@
 const axios = require("axios");
-const mongoose = require("mongoose");
+const quotesDB = require('./quotes.json');
+const chalk = require('chalk');
 require("dotenv").config();
+const yosay = require("yosay");
+const cowsay = require("cowsay");
+const chalkAnimation = require("chalk-animation");
+const fs = require("fs");
 
-const URL = process.env.MONGO_URL;
-mongoose
-  .connect(URL)
-  .then(() => console.log("Database Connected Successfully"))
-  .catch((err) => console.log(err));
+
+
+
+function print(q, a){
+chalkAnimation.rainbow("Loading...");
+setTimeout(() => {
+
+  console.log(chalk.underline.magenta.bold("\nStay Motivated...\n"));
+console.log(
+  yosay(
+    ["", "    " + chalk.green.bold(q), "        -- " + chalk.cyan(a), ""].join("\n")
+  )
+);
+}, 1000);
+}
+
+
+
+function appendQuote(obj, quotesDB){
+  var outputFile = "./quotes.json";
+  var j = 0;
+  for(j = 0; j < quotesDB.length; j++){
+    if(quotesDB[j]["quote"] == obj["quote"]) break;
+
+    if(j == quotesDB.length - 1) quotesDB.push(obj);
+  }
+   
+ 
+  fs.writeFile(outputFile, JSON.stringify(quotesDB, null, 4), function(err) {
+	    if(err) {
+	      console.log(err);
+	    } else {
+	     
+	    }
+   });
+ }
+
+function getAllQuotes(res){
+  var quote = res.data[0].q;
+  var auth = res.data[0].a;
+  
+
+  var obj = {
+    "quote": quote,
+    "author": auth
+  }
+  
+  
+  appendQuote(obj, quotesDB);
+  return obj; 
+
+}
+
+
+
+function randomIdx(len) {
+  return parseInt(Math.random() * 100) % len;
+}
+
+
+function printQuote(q, a) {
+ chalkAnimation.pulse("Loading Offline version...");
+ setTimeout(() => {
+   console.log(
+     cowsay.say({
+       text: ["", chalk.green.bold(q), "        -- " + chalk.cyan(a), ""].join(
+         "\n"
+       ),
+       e: "OO",
+       T: "U",
+     })
+   );
+ }, 1000);
+ 
+}
+
 const GetQuotes = (Online) => {
     if(Online){
-    axios.get("https://www.brainyquote.com/topics/motivational-quotes")
-         .then((res) => {
-             console.log(res);
-         })
-         .catch((err) => {
-             console.log(err);
-         });
+   
+   
+        axios
+          .get("https://zenquotes.io/api/random")
+          .then((res) => {
+        
+            var obj = getAllQuotes(res);
+            print(obj.quote, obj.author);
+       
+          })
+          .catch((err) => {
+            console.log(err);
+            const idx = randomIdx(quotesDB.length - 1);
+            print(quotesDB[idx]["quote"], quotesDB[idx]["author"]);
+          });
+
+
         }
         else{
-
+           
+           
+            var idx = randomIdx(quotesDB.length - 1);
+            printQuote(quotesDB[idx]["quote"], quotesDB[idx]["author"]);
+            
         }
   
 };
